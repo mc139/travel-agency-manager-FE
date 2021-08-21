34b5ef464c9badf53e7fc55da2690f3361447a5d
mc139 <maciejandrzejcieslik@gmail.com>
2021-08-21 18:19:06 +0100

    First Commit

---
+++ src/main/java/com/kodilla/travelagencymenagerfe/TravelAgencyMenagerFeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelagencymenagerfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyMenagerFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyMenagerFeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/travelagencymenagerfe/TravelAgencyMenagerFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelagencymenagerfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyMenagerFeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
